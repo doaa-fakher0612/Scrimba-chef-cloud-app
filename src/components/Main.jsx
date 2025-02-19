27adcb00c2d5d26c800f6a818a679ed9f4d656c8
doaa <doaafakher0612@gmail.com>
2025-02-19 15:39:05 +0200

    handel sumbmission using handeler not action

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function Main(){
-    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    const [ingredients, setIngredients] = useState([])
+    
 
     const ingredientListItems = ingredients.map ( ingredient => (
         <li key = {ingredient}> {ingredient}</li>
     ))
 
     function handelSubmit(event){
+          /**
+         * Like before, don't worry about this FormData stuff yet.
+         * Just use the newIngredient below to help you finish the
+         * challenge.
+         */
         event.preventDefault()
-        const formData  = new FormData(event.currentTarget);
+        const formData = new FormData(event.currentTarget)
         console.log(formData)
         const newIngredient = formData.get("ingredient")
-        
-        // ingredients.push(newIngredient)
-        console.log(ingredients)
-
-        
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient ])
 
         
     }
@@ -24,7 +26,7 @@ export default function Main(){
     return(
         <main>
         <form className="add-ing-form" onSubmit={handelSubmit}>
-            <input type="text" placeholder="e.g. oregano" aria-label="Add Ingrendient" name = "ingredient"/>
+            <input type="text" placeholder="e.g. oregano" aria-label="Add Ingredient" name = "ingredient"/>
             <button>Add Ingredient</button>
         </form>
         <ul>
