bb77f270d6cb17de597337fc8b7ecab082f5a410
doaa <doaafakher0612@gmail.com>
2025-04-09 18:08:41 +0200

    render CloudRecipe Component , Apply its Props

---
+++ src/components/ClaudeRecipe.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function ClaudeRecipe(props) {
+  return (
+    <section>
+      <h2>Ingredients on hand:</h2>
+      <ul className="ingredients-list" aria-live="polite">
+        {props.ingredientsListItems}
+      </ul>
+      {props.ingredients.length >= 4 && (
+        <div className="get-recipe-container">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button onClick={props.toggleRecipeShown}>Get a recipe</button>
+        </div>
+      )}
+    </section>
+  );
+}
